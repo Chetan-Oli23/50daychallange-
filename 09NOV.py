f3cdfbbcbe4aab0074830f6a9c06703fbfd37351
Chetan-Oli23 <Chetanoli800@gmail.com>
2025-11-11 23:20:51 +0545

    Update 09NOV.py

--- 09NOV.py
+++ 09NOV.py
@@ -1,8 +1,6 @@
 
-# name = input("Enter Name : ")
-# if name == "chetan" :
-#     print("Hello", name, "how are you")
-# print("How are you!!!")
+# if
+
 name = input("Enter Name : ")
 if name == "chetan" :
     print("Hello", name, "how are you")
@@ -11,12 +9,6 @@ print("How are you!!!")
 
 # if-else:
 
-# name = input("Enter Name : ")
-# if name == "chetan":
-#     print("Hello Chetan Good Morning ")
-# else:
-#     print("Hello Guest Good Morning")
-# print("How are you!!!")
 name = input("Enter Name : ")
 if name == "chetan":
     print("Hello Chetan Good Morning ")
@@ -26,37 +18,6 @@ print("How are you!!!")
 
 # if-elif-else:
 
-# brand = input("Enter Your Favourite Brand: ")
-# if brand == "CB":
-#     print("It is childrens brand")
-# elif brand =="KF":
-#     print("It is not that much kick: ")
-# elif brand == "KO":
-#     print("Buy one get Free One:")
-# else:
-#     print("Other Brands are not recommended ")
-
-
-# #1. Write a program to Check whether the give number is in butween 1 and 10
-
-# n =int(input("Enter the Number: "))
-# if n>0 and n<=10:
-#     print("The Number", n, "is between 1 and 10 ")
-# else:
-#     print("The number", n, " is not between 1 and 10")
-
-#2. Write a program to fine biggest of given 3 Number from the user 
-
-# n1=int(input("Enter Fist Number: "))
-# n2=int(input("Enter Second Number: "))
-# n3=int(input("Enter Third Number: "))
-
-# if n1>n2 and n1>n3:
-#     print("Biggest Number is ",n1)
-# elif n2>n3:
-#     print("Biggest Number is ",n2)
-# else:
-#     print("Biggest Number is ",n3)
 brand = input("Enter Your Favourite Brand: ")
 if brand == "CB":
     print("It is childrens brand")
